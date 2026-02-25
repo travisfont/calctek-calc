3f1ea7414b0c9661bf9ec46206833d478ddb7ca8
Travis van der Font <travis.font@gmail.com>
2026-02-25 15:24:36 +0100

    Initial Laravel + Vue.js skeleton project (latest versions)

---
+++ resources/js/components/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="min-h-screen flex items-center justify-center bg-gray-100">
+    <div class="text-center">
+      <h1 class="text-4xl font-bold text-gray-800 mb-4">Laravel + Vue 3</h1>
+      <p class="text-lg text-gray-600">Your application skeleton is ready.</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+}
+</script>
+
+<style scoped>
+/* Scoped styles here */
+</style>
